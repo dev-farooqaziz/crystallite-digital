2e4da5b01b37409b3d6b6449965d028ac5bf070d
cl-farooqaziz <dev.faz@production-dev.com>
2023-08-09 23:46:15 +0500

    mata tags

--- pages/about.js
+++ pages/about.js
@@ -7,12 +7,23 @@ export default function About() {
         <>
             <Head>
                 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-                <title>About - Crystallite Digital</title>
-                <link rel="icon" href="/images/logo.svg" sizes="32x32" title="logo" alt="favicon" />
-                <meta name="author" content="Alvaro Trigo Lopez" />
-                <meta name="description" content="fullPage full-screen navigation and sections control menu." />
-                <meta name="keywords" content="fullpage,jquery,demo,screen,fullscreen,navigation,control arrows, dots" />
                 <meta name="Resource-type" content="Document" />
+                {/* FavIcon */}
+                <link rel="icon" href="/images/logo.svg" sizes="32x32" title="logo" alt="favicon" />
+                {/* Meta Tags */}
+                <title>About - Crystallite Pvt Ltd</title>
+                <meta name="description" content="How We Meet Enterprise Needs We'll take care of business." />
+                <meta name="keywords" content="Digital Marketing, Seo, Smm, IT Services, Dedicated Hosting" />
+                <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+                {/* Og Tags */}
+                <meta property="og:locale" content="en_US" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="About - Crystallite Pvt Ltd" />
+                <meta property="og:description" content="How We Meet Enterprise Needs We'll take care of business." />
+                <meta property="og:url" content="https://crystallite-digital.vercel.app/about" />
+                <meta property="og:site_name" content="About - Crystallite Pvt Ltd" />
+                {/* canonical */}
+                <link rel="canonical" href="https://crystallite-digital.vercel.app/about" />
             </Head>
             <FullPage />
         </>
--- pages/managed-it.js
+++ pages/managed-it.js
@@ -7,12 +7,23 @@ export default function ManagedIT() {
         <>
             <Head>
                 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-                <title>Managed IT - Crystallite Digital</title>
-                <link rel="icon" href="/images/logo.svg" sizes="32x32" title="logo" alt="favicon" />
-                <meta name="author" content="Alvaro Trigo Lopez" />
-                <meta name="description" content="fullPage full-screen navigation and sections control menu." />
-                <meta name="keywords" content="fullpage,jquery,demo,screen,fullscreen,navigation,control arrows, dots" />
                 <meta name="Resource-type" content="Document" />
+                {/* FavIcon */}
+                <link rel="icon" href="/images/logo.svg" sizes="32x32" title="logo" alt="favicon" />
+                {/* Meta Tags */}
+                <title>Managed IT - Crystallite Pvt Ltd</title>
+                <meta name="description" content="How We Meet Enterprise Needs We'll take care of business." />
+                <meta name="keywords" content="Digital Marketing, Seo, Smm, IT Services, Dedicated Hosting" />
+                <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+                {/* Og Tags */}
+                <meta property="og:locale" content="en_US" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Managed IT - Crystallite Pvt Ltd" />
+                <meta property="og:description" content="How We Meet Enterprise Needs We'll take care of business." />
+                <meta property="og:url" content="https://crystallite-digital.vercel.app/managed-it" />
+                <meta property="og:site_name" content="Managed IT - Crystallite Pvt Ltd" />
+                {/* canonical */}
+                <link rel="canonical" href="https://crystallite-digital.vercel.app/managed-it" />
             </Head>
             <FullPage />
         </>
