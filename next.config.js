62aebc265f1b75a79c97cfe651f37978ebdf6bb6
cl-farooqaziz <dev.faz@production-dev.com>
2023-10-02 11:24:55 -0700

    image error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
   unoptimized: true,
 }
 
 module.exports = nextConfig
-
